388837987110526b7138db69478d97a6f9c75291
azhar <kaziharkanneth@gmail.com>
2024-05-11 20:39:22 +0530

    convert UserList into functional component - change witUserData HOC into hook

---
+++ components/hooks/useWithUserData.js
@@ -0,0 +1,95 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+
+const columnFields = [
+  { value: 'id', label: 'Id' },
+  { value: 'name', label: 'Name', enableSearch: true },
+  { value: 'email', label: 'Email', enableSearch: true },
+  { value: 'username', label: 'Username' },
+  { value: 'phone', label: 'Phone' },
+  { value: 'website', label: 'Website' },
+];
+
+const useWithUserData = () => {
+  const [users, setUsers] = useState([]);
+  const [filteredUsers, setFilteredUsers] = useState([]);
+  const [searchName, setSearchName] = useState('');
+  const [searchEmail, setSearchEmail] = useState('');
+  const [sortColumn, setSortColumn] = useState(columnFields[0].value);
+  const [sortDirection, setSortDirection] = useState('asc');
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        const { data: usersData } = await axios.get('/api/v1/users');
+        setUsers(usersData);
+        setFilteredUsers(usersData);
+      } catch (error) {
+        console.error('Error fetching users:', error);
+      }
+    };
+
+    fetchUsers();
+
+    // Cleanup function
+    return () => {
+      // Any cleanup code here
+    };
+  }, []); // Empty dependency array ensures the effect runs only once
+
+  useEffect(() => {
+    const filterAndSortUsers = () => {
+      let filteredUsersResult = users.filter(
+        user =>
+          user.name.toLowerCase().includes(searchName.toLowerCase()) &&
+          user.email.toLowerCase().includes(searchEmail.toLowerCase()),
+      );
+
+      if (sortColumn) {
+        filteredUsersResult.sort((a, b) => {
+          const x = a[sortColumn];
+          const y = b[sortColumn];
+          return sortDirection === 'asc' ? (x < y ? -1 : 1) : x > y ? -1 : 1;
+        });
+      }
+
+      setFilteredUsers(filteredUsersResult);
+    };
+
+    filterAndSortUsers();
+  }, [users, searchName, searchEmail, sortColumn, sortDirection]);
+
+  const handleOnSearch = event => {
+    const { name, value } = event.target;
+
+    if (name === 'name') {
+      setSearchName(value);
+    } else if (name === 'email') {
+      setSearchEmail(value);
+    } else {
+      throw new Error('Unknown search element');
+    }
+  };
+
+  const handleSort = column => {
+    if (sortColumn === column) {
+      setSortDirection(prevDirection =>
+        prevDirection === 'asc' ? 'desc' : 'asc',
+      );
+    } else {
+      setSortColumn(column);
+      setSortDirection('asc');
+    }
+  };
+
+  return {
+    users: filteredUsers,
+    handleOnSearch,
+    handleSort,
+    columnFields,
+    sortColumn,
+    sortDirection,
+  };
+};
+
+export default useWithUserData;
